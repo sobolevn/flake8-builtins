f304b8d4818a6cac0e1c2654ed6abdd084a64e7c
Gil Forcada <gforcada@gnome.org>
2016-03-02 22:40:54 +0100

    Actual code

--- flake8_builtins.py
+++ flake8_builtins.py
@@ -1,17 +1,60 @@
 # -*- coding: utf-8 -*-
+import __builtin__
+import ast
+import inspect
+
+
+BUILTINS = [a[0] for a in inspect.getmembers(__builtin__)]
+
 
 class BuiltinsChecker(object):
     name = 'flake8_builtins'
     version = '0.1'
+    assign_msg = 'B001 "{0}" is a python builtin and is being shadowed, ' \
+                 'consider renaming the variable'
+    argument_msg = 'B002 "{0}" is used as an argument and thus shadows a ' \
+                   'python builtin, consider renaming the argument'
 
     def __init__(self, tree, filename):
         self.tree = tree
         self.filename = filename
-        self.builtins = self.get_builtins()
 
     def run(self):
-        pass
+        for statement in ast.walk(self.tree):
+            if isinstance(statement, ast.Assign):
+                value = self.check_assignment(statement)
+                if value:
+                    yield value
 
+            if isinstance(statement, ast.FunctionDef):
+                value = self.check_function_definition(statement)
+                if value:
+                    yield value
 
-    def get_builtins(self):
-        return dir(__builtins__)
+    def check_assignment(self, statement):
+        for element in statement.targets:
+            if isinstance(element, ast.Name) and \
+                    element.id in BUILTINS:
+
+                line = element.lineno
+                offset = element.col_offset
+                return (
+                    line,
+                    offset,
+                    self.assign_msg.format(element.id),
+                    type(self)
+                )
+
+    def check_function_definition(self, statement):
+        for arg in statement.args.args:
+            if isinstance(arg, ast.Name) and \
+                    arg.id in BUILTINS:
+
+                line = arg.lineno
+                offset = arg.col_offset
+                return (
+                    line,
+                    offset,
+                    self.argument_msg.format(arg.id),
+                    type(self)
+                )
--- run_tests.py
+++ run_tests.py
@@ -2,22 +2,99 @@
 from flake8_builtins import BuiltinsChecker
 from tempfile import mkdtemp
 
-import os
+import ast
 import unittest
 
 
 class TestBuiltins(unittest.TestCase):
 
-    def _given_a_file_in_test_dir(self, contents):
-        test_dir = os.path.realpath(mkdtemp())
-        file_path = os.path.join(test_dir, 'test.py')
-        with open(file_path, 'w') as a_file:
-            a_file.write(contents)
+    def test_builtin_top_level(self):
+        tree = ast.parse('max = 4')
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertEqual(len(ret), 1)
 
-        return file_path
+    def test_nested(self):
+        tree = ast.parse(
+            'def bla():\n'
+            '    filter = 4'
+        )
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertEqual(len(ret), 1)
 
-    def test_dummy(self):
-        pass
+    def test_more_nested(self):
+        tree = ast.parse(
+            'class Bla(object):\n'
+            '    def method(self):\n'
+            '        int = 4'
+        )
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertEqual(len(ret), 1)
+
+    def test_line_number(self):
+        tree = ast.parse(
+            'a = 2\n'
+            'open = 4'
+        )
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertEqual(ret[0][0], 2)
+
+    def test_offset(self):
+        tree = ast.parse(
+            'def bla():\n'
+            '    zip = 4'
+        )
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertEqual(ret[0][1], 4)
+
+    def test_assign_message(self):
+        tree = ast.parse('def bla():\n    object = 4')
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertTrue(ret[0][2].startswith('B001 '))
+
+    def test_argument_message(self):
+        tree = ast.parse('def bla(list):\n    a = 4')
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertTrue(ret[0][2].startswith('B002 '))
+
+    def test_keyword_argument_message(self):
+        tree = ast.parse('def bla(dict=3):\n    b = 4')
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertTrue(ret[0][2].startswith('B002 '))
+
+    def test_no_error(self):
+        tree = ast.parse('def bla(first):\n    b = 4')
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertEqual(
+            len(ret),
+            0
+        )
+
+    def test_method_without_arguments(self):
+        tree = ast.parse('def bla():\n    b = 4')
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertEqual(
+            len(ret),
+            0
+        )
+
+    def test_method_only_normal_keyword_arguments(self):
+        tree = ast.parse('def bla(k=4):\n    b = 4')
+        checker = BuiltinsChecker(tree, '/home/script.py')
+        ret = [c for c in checker.run()]
+        self.assertEqual(
+            len(ret),
+            0
+        )
 
 
 if __name__ == '__main__':
